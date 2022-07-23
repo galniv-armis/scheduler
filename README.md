34912a14f9211019424f4efd80862c7053029298
Gal Niv <galniv.work@gmail.com>
2022-07-23 23:14:08 +0300

    Update README.md

--- README.md
+++ README.md
@@ -1,16 +1,89 @@
-## Introduction
+# Introduction
+A simple scheduler service api server implemented in Typescript using Node.js & MongoDB.
+The server implements a `REST api` for `CRUD` operations to `scheduled-jobs`.
 
-Welcome to this MongoDB and TypeScript sample project.
-The aim of this project is to give you a working example of how you can use the power of MongoDB Atlas with TypeScript and Express to create modern web applications.
+You're welcome to use the deployed service available in: https://a-team-scheduler-api.herokuapp.com/
 
-## Installation & Configuration
+See documentation below for more details of how to use the api.
 
+# Thought & Ideas For Future Development
+## Features & Code Refactors
+1. Add a UI to the service.
+2. Support authentication of users & role based permissions.
+3. Add a separate interface for Jobs & extend it in ScheduledJobs (as I later realized the immediate jobs don't really belong to this type).
+
+## Testing & CI
+1. Improve the unit tests to not really wait for crons to fire.
+2. Add integration tests.
+
+## Scale
+In case the amount of data is too large to fit in memory the service supports raising more instances, each instance can manage a subset of the data using its data provider.
+
+Another approach could be to only save ids & crons for a job & delegate the job execution to another serve. 
+
+
+# API documentation
+```
+/scheduled-jobs
+GET - gets all scheduled jobs.
+POST - Add a scheduled job the schema for a job is:
+        {
+            title: string,
+            description: string,
+            jobType: oneOf("MAKE_COFFEE", "EMAIL", "BUY_PLANE_TICKET", "BUILD_PROJECT")
+            cronTime: A valid cron string or a date javascript can parse using its built-in Date object.
+          
+            // not required
+            jobParams: object - config for the specific job
+        }
+
+/scheduled-jobs/id
+GET - Gets the scheudled job with the given id.
+PUT - Updates the scheduled job with the given id if exists using a partial (or full) scheduled job from the schema above.
+DELETE - Deltes the scheduled job with the given id 
+
+All API endpoints status codes adhere the conventions for REST api.
+```
+
+## Examples For Scheduled Jobs
+```
+// Runs every second
+{
+    "title": "Make Coffee Every Second",
+    "description": "Make coffee with 2 spoons of sugar every second!",
+    "jobType": "MAKE_COFFEE",
+    "jobParams": {"sugar": 2},
+    "cronTime": "* * * * * *"
+}
+
+// Runs on 17/12/2023
+{
+    "title": "Buy a ticket to Taiwan",
+    "description": "But me a ticket using my credit card.",
+    "jobType": "BUY_PLANE_TICKET",
+    "jobParams": {"creditCardInfo": "maybe not a good idea to include here"}
+}
+
+// Immediate 
+{
+    "title": "Send Joe an Email",
+    "description": "Send an email to Joe immediately.",
+    "jobType": "EMAIL",
+    "jobParams": {"to": "joe@....", "content": "An email."}
+    
+}
+
+
+```
+
+# Running Locally
+
+# Installation & Configuration
 1. Create a `.env` file in the root of the project with the properties I've provided separately.
-2. Run `npm i` to install all dependencies listed in `package.json`
-
+2. Run `npm i` to install all dependencies listed in `package.json`.
 
 # Run Dev Server 
-`npm start`
+`npm run dev`
 
 # Run Tests
 `npm test` 
--- src/services/scheduledJobs.service.ts
+++ src/services/scheduledJobs.service.ts
@@ -5,14 +5,12 @@ import {ScheduledJob, JobType} from "../models/scheduledJob";
 
 type DataProvider = Collection<ScheduledJob> // | more types
 
-
-export const JOB_TYPE_TO_ACTION = new Map<JobType, (scheduledJobParams: object) => void>([
+const JOB_TYPE_TO_ACTION = new Map<JobType, (scheduledJobParams: object) => void>([
     [JobType.Email, (scheduledJobParams: object) => console.log(`Sending email with config  ${JSON.stringify(scheduledJobParams)}`)],
     [JobType.BuildProject, (scheduledJobParams: object) => console.log(`Building a project with config ${JSON.stringify(scheduledJobParams)}`)],
     [JobType.MakeCoffee, (scheduledJobParams: object) => console.log(`Making coffee with config ${JSON.stringify(scheduledJobParams)}`)],
     [JobType.BuyPlainTicket, (scheduledJobParams: object) => console.log(`Buying a plane ticket with config ${JSON.stringify(scheduledJobParams)}`)],
 ])
-
 
 class ScheduledJobsService {
     _scheduledJobs: Map<string, CronJob>;
@@ -23,13 +21,13 @@ class ScheduledJobsService {
     }
 
     async setup(dataProvider: DataProvider) {
-        this._dataProvider = dataProvider
+        this._dataProvider = dataProvider;
 
         const activeJobsQuery: Filter<ScheduledJob> = {$or: [{cronTime: {$gt: new Date()}}, {cronTime: {$type: "string"}}]};
         const scheduledJobs = await this.getAllAsArray(activeJobsQuery);
         scheduledJobs.forEach((scheduledJob: ScheduledJob) => {
             try {
-                this.setCron(scheduledJob)
+                this.setCron(scheduledJob);
             } catch (e) {
                 console.log(`Failed to schedule job with id: ${scheduledJob._id}`)
                 console.log(e);
@@ -50,26 +48,27 @@ class ScheduledJobsService {
         newScheduledJob = transformCronTimeIfNeeded(newScheduledJob);
         const result = await this._dataProvider.insertOne(newScheduledJob);
         if (result) {
-            this.setCron({...newScheduledJob, _id: new ObjectId(result.insertedId)})
+            this.setCron({...newScheduledJob, _id: new ObjectId(result.insertedId)});
         }
-        return result
+        return result;
     }
 
     async update(updatedScheduledJob: ScheduledJob) {
-        const query = {_id: updatedScheduledJob._id}
+        updatedScheduledJob = transformCronTimeIfNeeded(updatedScheduledJob);
+        const query = {_id: updatedScheduledJob._id};
         const result = await this._dataProvider.updateOne(query, {$set: updatedScheduledJob});
         if (result) {
-            this.setCron(updatedScheduledJob)
+            this.setCron(updatedScheduledJob);
         }
-        return result
+        return result;
     }
 
     async delete(scheduledJobId: ObjectId) {
         const result = await this._dataProvider.deleteOne({_id: scheduledJobId});
         if (result && result.deletedCount) {
-            this.deleteCron(scheduledJobId.toString())
+            this.deleteCron(scheduledJobId.toString());
         }
-        return result
+        return result;
     }
 
     dispatchImmediate(newScheduledJob: ScheduledJob) {
@@ -80,7 +79,7 @@ class ScheduledJobsService {
     private setCron(scheduledJob: Partial<ScheduledJob>) {
         const scheduledJobId = scheduledJob._id.toString();
         const existingJob = this._scheduledJobs.get(scheduledJobId);
-        existingJob?.stop()
+        existingJob?.stop();
         const newCronJob = new CronJob(scheduledJob.cronTime, () => {
             JOB_TYPE_TO_ACTION.get(scheduledJob.jobType)(scheduledJob.jobParams);
             this.deleteCronIfDue(newCronJob, scheduledJobId);
@@ -90,7 +89,7 @@ class ScheduledJobsService {
 
     private deleteCronIfDue(newCronJob: CronJob, scheduledJobId: string) {
         try {
-            newCronJob.nextDates()
+            newCronJob.nextDates();
         } catch (e) {
             this.deleteCron(scheduledJobId);
         }
@@ -98,18 +97,21 @@ class ScheduledJobsService {
 
     private deleteCron(scheduledJobId: string) {
         const existingJob = this._scheduledJobs.get(scheduledJobId);
-        existingJob?.stop()
+        existingJob?.stop();
         this._scheduledJobs.delete(scheduledJobId);
     }
 }
 
 
-const transformCronTimeIfNeeded = (newScheduledJob: ScheduledJob) => {
-    let cronTime = newScheduledJob.cronTime;
-    if (typeof cronTime === "string" && Date.parse(cronTime)) {
-        cronTime = new Date(cronTime);
+const transformCronTimeIfNeeded = (scheduledJob: ScheduledJob) => {
+    if (!scheduledJob.cronTime) {
+        return scheduledJob;
     }
-    return {...newScheduledJob, cronTime};
+
+    const cronTime = (typeof scheduledJob.cronTime === "string" && Date.parse(scheduledJob.cronTime))
+        ? new Date(scheduledJob.cronTime)
+        : scheduledJob.cronTime;
+    return {...scheduledJob, cronTime};
 }
 
 export const scheduledJobsService = new ScheduledJobsService();
